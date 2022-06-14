ef9db7113bdffe35a30a365dcf80efb011baefa6
TekyRey <mwakabayah@gmail.com>
2022-06-14 12:34:22 +0300

    fix linter errors

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const openMobileMenu = document.querySelector(".menu-icon img");
-const closeMobileMenu = document.querySelector(".mobile-menu button");
-const mobileMenuItems = document.querySelectorAll(".mobile-items a");
+const openMobileMenu = document.querySelector('.menu-icon img');
+const closeMobileMenu = document.querySelector('.mobile-menu button');
+const mobileMenuItems = document.querySelectorAll('.mobile-items a');
 
-openMobileMenu.addEventListener("click", () => {
-    document.querySelector(".mobile-menu").classList.add("show-menu");
+openMobileMenu.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.add('show-menu');
 });
 
-closeMobileMenu.addEventListener("click", () => {
-  document.querySelector(".mobile-menu").classList.remove("show-menu");
+closeMobileMenu.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.remove('show-menu');
 });
 
 mobileMenuItems.forEach((item) => {
-    item.addEventListener("click", () => {
-        document.querySelector(".mobile-menu").classList.remove("show-menu");
+    item.addEventListener('click', () => {
+        document.querySelector('.mobile-menu').classList.remove('show-menu');
     });
 })
-
-
-
-
-
