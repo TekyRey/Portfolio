648cc0d0d8de5a068b7b38faa7c6847ec9ec2f2f
Thokozan Ngubane <mxolisithokozani7@gmail.com>
2022-06-14 10:08:01 +0200

    added responsive code

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const openMobileMenu = document.querySelector(".menu-icon img");
+const closeMobileMenu = document.querySelector(".mobile-menu button");
+const mobileMenuItems = document.querySelectorAll(".mobile-items a");
+
+openMobileMenu.addEventListener("click", () => {
+    document.querySelector(".mobile-menu").classList.add("show");
+});
+
+closeMobileMenu.addEventListener("click", () => {
+  document.querySelector(".mobile-menu").classList.remove("show");
+});
+
+mobileMenuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        document.querySelector(".mobile-menu").classList.remove("show");
+    });
+})
 
 
 
