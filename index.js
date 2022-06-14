d524e1f40b944b25280634b46371d80c787fbe85
TekyRey <mwakabayah@gmail.com>
2022-06-14 11:11:12 +0300

    styled mobile menu

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const closeMobileMenu = document.querySelector(".mobile-menu button");
 const mobileMenuItems = document.querySelectorAll(".mobile-items a");
 
 openMobileMenu.addEventListener("click", () => {
-    document.querySelector(".mobile-menu").classList.add("show");
+    document.querySelector(".mobile-menu").classList.add("show-menu");
 });
 
 closeMobileMenu.addEventListener("click", () => {
-  document.querySelector(".mobile-menu").classList.remove("show");
+  document.querySelector(".mobile-menu").classList.remove("show-menu");
 });
 
 mobileMenuItems.forEach((item) => {
     item.addEventListener("click", () => {
-        document.querySelector(".mobile-menu").classList.remove("show");
+        document.querySelector(".mobile-menu").classList.remove("show-menu");
     });
 })
 
