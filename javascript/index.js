43b37f240d90ca1b6f5f45acb4dea56e42f0dba6
TekyRey <mwakabayah@gmail.com>
2022-06-17 12:55:29 +0300

    resized mobile-popup

--- javascript/index.js
+++ javascript/index.js
@@ -146,7 +146,13 @@ submitButton.forEach((button) => {
     <div class="popup-text">
         <p class="desktop-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s
-             with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+             with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+             the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+            it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+             printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and
+            scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry
+            Lorem Ipsum has been the industry's standard dummy 
+            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
 
           <p class="mobile-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
           standard dummy text ever since the 1500s</p>
