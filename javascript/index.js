947bf112f218acad0b0e00d7370d4d51035a2e0c
TekyRey <mwakabayah@gmail.com>
2022-06-17 03:13:55 +0300

    add style to popup buttons

--- javascript/index.js
+++ javascript/index.js
@@ -113,13 +113,7 @@ submitButton.forEach((button) => {
     <div class="popup-text">
         <p class="desktop-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s
-             with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-             the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
-            it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-             printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and
-            scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry
-            Lorem Ipsum has been the industry's standard dummy 
-            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+             with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
 
           <p class="mobile-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
           standard dummy text ever since the 1500s</p>
