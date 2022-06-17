02a30f030df5dfaff5e86cab2a6f3a19d453557d
TekyRey <mwakabayah@gmail.com>
2022-06-17 13:02:26 +0300

    fix linter errors

--- javascript/index.js
+++ javascript/index.js
@@ -18,20 +18,19 @@ mobileMenuItems.forEach((item) => {
 
 // Form Validation
 
-// const formInput = document.getElementById("contact-form");
-// const email = document.querySelector("#email");
-// const errorElement = document.getElementById("error");
-// errorElement.style.color = "red";
+// const formInput = document.getElementById('contact-form');
+// const email = document.querySelector('#email');
+// const errorElement = document.getElementById('error');
+// errorElement.style.color = 'red';
 
-// formInput.addEventListener("submit", (e) => {
+// formInput.addEventListener('submit', (e) => {
 //   e.preventDefault();
 //   if (email.value === email.value.toLowerCase()) {
 //     formInput.submit();
 //   } else {
-//     errorElement.innerText = "Please type your e-mail in lowercase";
+//     errorElement.innerText = 'Please type your e-mail in lowercase';
 //   }
 // });
-
 
 // Popup window
 
@@ -39,76 +38,76 @@ const portfolio = document.querySelector('#portfolio');
 
 const projectArray = [
   {
-    mobileImg: "images/tonic-section.svg",
-    desktopImg: "images/desktop-snap3.png",
-    cardTitle: "Tonic",
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap3.png',
+    cardTitle: 'Tonic',
     cardDesc:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
-    languages: ["html", "css", "JavaScript"],
-    liveLink: "https://TekyRey.github.io/Portfolio/",
-    github: "https://github.com/TekyRey/Portfolio",
-    class: "card",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    languages: ['html','css','Javascript'],
+    liveLink: 'https://TekyRey.github.io/Portfolio/',
+    github: 'https://github.com/TekyRey/Portfolio',
+    class: 'card',
   },
 
   {
-    mobileImg: "images/availability-section.svg",
-    desktopImg: "images/desktop-snap2.png",
-    cardTitle: "Multi-Post Stories",
+    mobileImg: 'images/availability-section.svg',
+    desktopImg: 'images/desktop-snap2.png',
+    cardTitle: 'Multi-Post Stories',
     cardDesc:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
-    languages: ["html", "css", "JavaScript"],
-    liveLink: "https://TekyRey.github.io/Portfolio/",
-    github: "https://github.com/TekyRey/Portfolio",
-    class: "card-2",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    languages: ['html','css','Javascript'],
+    liveLink: 'https://TekyRey.github.io/Portfolio/',
+    github: 'https://github.com/TekyRey/Portfolio',
+    class: 'card-2',
   },
 
   {
-    mobileImg: "images/tonic-section.svg",
-    desktopImg: "images/desktop-snap4.png",
-    cardTitle: "Tonic",
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap4.png',
+    cardTitle: 'Tonic',
     cardDesc:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
-    languages: ["html", "css", "JavaScript"],
-    liveLink: "https://TekyRey.github.io/Portfolio/",
-    github: "https://github.com/TekyRey/Portfolio",
-    class: "card",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    languages: ['html','css','Javascript'],
+    liveLink: 'https://TekyRey.github.io/Portfolio/',
+    github: 'https://github.com/TekyRey/Portfolio',
+    class: 'card',
   },
 
   {
-    mobileImg: "images/tonic-section.svg",
-    desktopImg: "images/desktop-snap3.png",
-    cardTitle: "Tonic",
+    mobileImg: 'images/tonic-section.svg',
+    desktopImg: 'images/desktop-snap3.png',
+    cardTitle: 'Tonic',
     cardDesc:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required",
-    languages: ["html", "css", "JavaScript"],
-    liveLink: "https://TekyRey.github.io/Portfolio/",
-    github: "https://github.com/TekyRey/Portfolio",
-    class: "card-2",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    languages: ['html','css','Javascript'],
+    liveLink: 'https://TekyRey.github.io/Portfolio/',
+    github: 'https://github.com/TekyRey/Portfolio',
+    class: 'card-2',
   },
 ];
 
 portfolio.innerHTML = projectArray.map((project) => {
   const html = `
-  <section class="card ${project.class} work">
-  <img class= "mobile-img" src="${project.mobileImg}" alt="" aria-hidden="true">
-  <img class= "desktop-img" src="${
-    project.desktopImg
-  }" alt="" aria-hidden="true">
+  <section class='card ${project.class} work'>
+  <img class= 'mobile-img' src='${project.mobileImg}' alt='' aria-hidden='true'>
+  <img class= 'desktop-img' src='${
+  project.desktopImg
+}' alt='' aria-hidden='true'>
 
-  <div class="content">
+  <div class='content'>
       <h2>${project.cardTitle}</h2>
-      <div class="experience-section">
-          <p class="canopy">Canopy</p>
-          <img src="images/Counter.svg" alt="" aria-hidden="true">
-          <p class="bullet">Back End Dev</p>
-          <img src="images/Counter.svg" alt="" aria-hidden="true">
-          <p class="bullet">2015</p>
+      <div class='experience-section'>
+          <p class='canopy'>Canopy</p>
+          <img src='images/Counter.svg' alt='' aria-hidden='true'>
+          <p class='bullet'>Back End Dev</p>
+          <img src='images/Counter.svg' alt='' aria-hidden='true'>
+          <p class='bullet'>2015</p>
       </div>
-      <p class="card-text">${project.cardDesc}</p>
-      <ul class="skills">
-          ${project.languages.map((x) => `<li>${x}</li>`).join("")}
+      <p class='card-text'>${project.cardDesc}</p>
+      <ul class='skills'>
+          ${project.languages.map((x) => `<li>${x}</li>`).join('')}
       </ul>
-      <button class="see-project submit" type="submit">See Project</button>
+      <button class='see-project submit' type='submit'>See Project</button>
   </div>
 </section>
   `;
@@ -124,27 +123,27 @@ submitButton.forEach((button) => {
     popup.classList.remove('hide');
     mainContent.classList.add('blur');
     popup.innerHTML = `
-    <div class="popup-header">
+    <div class='popup-header'>
     <h2>Tonic</h2>
-    <span class="cancel">&times;</span>
+    <span class='cancel'>&times;</span>
 </div>
 
-<div class="experience-section">
-    <p class="mobile-only canopy">Canopy</p>
-    <p class="desktop-only canopy">Uber</p>
-    <img src="images/Counter.svg" alt="" aria-hidden="true">
-    <p class="mobile-only bullet">Back End Dev</p>
-    <p class="desktop-only bullet">Lead Developer</p>
-    <img src="images/Counter.svg" alt="" aria-hidden="true">
-    <p class="mobile-only bullet">2015</p>
-    <p class="desktop-only bullet">2018</p>
+<div class='experience-section'>
+    <p class='mobile-only canopy'>Canopy</p>
+    <p class='desktop-only canopy'>Uber</p>
+    <img src='images/Counter.svg' alt='' aria-hidden='true'>
+    <p class='mobile-only bullet'>Back End Dev</p>
+    <p class='desktop-only bullet'>Lead Developer</p>
+    <img src='images/Counter.svg' alt='' aria-hidden='true'>
+    <p class='mobile-only bullet'>2015</p>
+    <p class='desktop-only bullet'>2018</p>
 </div>
 
-<img class="popup-hero-img" src="images/popup-img.png" alt="">
+<img class='popup-hero-img' src='images/popup-img.png' alt=''>
 
-<div class="popup-content">
-    <div class="popup-text">
-        <p class="desktop-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+<div class='popup-content'>
+    <div class='popup-text'>
+        <p class='desktop-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s
              with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
@@ -154,20 +153,20 @@ submitButton.forEach((button) => {
             Lorem Ipsum has been the industry's standard dummy 
             text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
 
-          <p class="mobile-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+          <p class='mobile-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
           standard dummy text ever since the 1500s</p>
     </div>
     <div>
-        <ul class="skills">
+        <ul class='skills'>
             <li>html</li>
-            <li class="desktop-only">Ruby on rails</li>
+            <li class='desktop-only'>Ruby on rails</li>
             <li>css</li>
             <li>javaScript</li>
         </ul>
         
-        <div class="popup-buttons">
-            <button>See Live <img src="images/see-more.png" alt=""></button>
-            <button>See More <img src="images/angelist.png" alt=""></button>
+        <div class='popup-buttons'>
+            <button>See Live <img src='images/see-more.png' alt=''></button>
+            <button>See More <img src='images/angelist.png' alt=''></button>
         </div>
     </div>
 </div>
